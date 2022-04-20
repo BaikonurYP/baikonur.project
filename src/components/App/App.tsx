a5f038f9b90180d4877f505d96789d1198e91f12
imDok <denekin.nikita@bk.ru>
2022-04-21 01:49:40 +0700

    Feat: добавлен роутер

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,43 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react'
+import { Route, Switch } from 'react-router-dom'
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Мой апп.</h1>
-            </div>
-        );
-    }
+import LoginPage from '../../pages/LoginPage/LoginPage'
+import SignupPage from '../../pages/SignupPage/SignupPage'
+import HamePage from '../../pages/HomePage/HomePage'
+import ProfilePage from '../../pages/ProfilePage/ProfilePage'
+import ForumPage from '../../pages/ForumPage/ForumPage'
+import LeaderboardPage from '../../pages/LeaderboardPage/LeaderboardPage'
+import Page404 from '../../pages/404Page/404Page'
+import Page500 from '../../pages/500Page/500Page'
+
+const App: FC = () => {
+    return (
+        <Switch>
+            <Route path="/login">
+                <LoginPage />
+            </Route>
+            <Route path="/signup">
+                <SignupPage />
+            </Route>
+            <Route path="/home">
+                <HamePage />
+            </Route>
+            <Route path="/profile">
+                <ProfilePage />
+            </Route>
+            <Route path="/forum">
+                <ForumPage />
+            </Route>
+            <Route path="/leaderboard">
+                <LeaderboardPage />
+            </Route>
+            <Route path="*">
+                <Page404></Page404>
+            </Route>
+            <Route path="/500">
+                <Page500></Page500>
+            </Route>
+        </Switch>
+    )
 }
-export default App;
+export default App
