00bd6d3b118ac4f5c17831451a27477ac2e8c636
Elena Pavlovskaya <ddpinzzz@gmail.com>
2022-04-20 16:06:26 +0300

    #task_2 добавлена обработка картинок и стилей в webpack, изменение правил

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react'
+import styled from 'styled-components'
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Мой апп.</h1>
-            </div>
-        );
-    }
-}
-export default App;
+import '../../styles/main.scss'
+
+export const Wrapper = styled.div`
+    width: 60px;
+    height: 50px;
+    border: 1px solid black;
+`
+
+const App: FC = () => (
+    <div>
+        <Wrapper />
+        <h1>Мdwqdqwй апп.</h1>
+    </div>
+)
+export default App
