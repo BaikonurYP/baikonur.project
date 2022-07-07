2e5ac9459e699943024435b0d174b028b645b478
Elena Pavlovskaya <ddpinzzz@gmail.com>
2022-07-07 10:12:36 +0300

    no message

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
+
 const https = require('https')
 const { join } = require('path')
 const { readFileSync } = require('fs')
+const Loadable = require('react-loadable')
 
 const { app, sequelize } = require('./dist/server.js')
 
@@ -12,21 +14,25 @@ const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        if (process.env.NODE_ENV === 'development') {
-            const key = readFileSync(join(__dirname, '/src/server/certs', 'key.pem'), 'utf8')
-            const options = {
-                key: readFileSync(join(__dirname, '/src/server/certs', 'key.pem'), 'utf8'),
-                cert: readFileSync(join(__dirname, '/src/server/certs', 'cert.pem'), 'utf8'),
-            };
+        Loadable.preloadAll().then(() => {
+            if (process.env.NODE_ENV === 'development') {
+                const key = readFileSync(join(__dirname, '/src/server/certs', 'key.pem'), 'utf8')
+                const options = {
+                    key: readFileSync(join(__dirname, '/src/server/certs', 'key.pem'), 'utf8'),
+                    cert: readFileSync(join(__dirname, '/src/server/certs', 'cert.pem'), 'utf8'),
+                    passphrase: 'root'
+                };
 
-            https.createServer(options, app).listen(port, () => {
-                console.log('Application is started on', `https://local.ya-praktikum.tech:${port}/`);
-            });
-            return;
-        }
-        app.listen(port, () => {
-            console.log(`Application is started on localhost:${port}`)
-        })
+                https.createServer(options, app).listen(port, () => {
+                    console.log('Application is started on', `https://local.ya-praktikum.tech:${port}/`);
+                });
+                return;
+            }
+            app.listen(port, () => {
+                console.log(`Application is started on localhost:${port}`)
+            })
+        });
+
     } catch (e) {
         console.error(e)
     }
